e098a451c5ffe1837d75648733ec8898bb582021
李航 <1943759704@qq.com>
2018-03-26 14:22:26 +0800

    model add

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from blog.models import article
+
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'cover_img',
+        'category_id',
+        'user_id',
+        'user_name',
+        'click_count',
+        'comment_count',
+        'praise_count',
+        'sort',
+        'is_show',
+        'is_hot',
+        'is_top',
+        'is_essence',
+        'description',
+        'content',
+        'created_at'
+    ]
+
+
+admin.site.register(article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
 class article(models.Model):
     title = models.CharField(max_length=150)
-    cover_img = ''
-    category_id = ''
+    cover_img = models.ImageField(max_length=200)
+    category_id = models.PositiveIntegerField()
+    user_id = models.PositiveIntegerField()
+    user_name = models.CharField(max_length=25)
+    click_count = models.PositiveIntegerField()
+    comment_count = models.PositiveIntegerField()
+    praise_count = models.PositiveIntegerField()
+    sort = models.PositiveSmallIntegerField()
+    is_show = models.BooleanField()
+    is_hot = models.BooleanField()
+    is_top = models.BooleanField()
+    is_essence = models.BooleanField()
     description = models.TextField()
     content = models.TextField()
-    cate = models.TextField()
+    created_at = models.DateTimeField()
