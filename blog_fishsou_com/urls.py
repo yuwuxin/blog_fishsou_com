6c1b61a7fd1a50301245867002bbebc7e1808ad8
李航 <1943759704@qq.com>
2018-04-03 14:14:10 +0800

    pic uploads

--- blog_fishsou_com/urls.py
+++ blog_fishsou_com/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('article-<int:id>.html', views.article),
     path('about', views.about),
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 
 # +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
